4d672a0dc5ec265125e81288f747ca67ce22ba7f
Vishnu <pvishnuprasaath@gmail.com>
2017-09-10 07:06:52 +0530

    GET request for decryption handled

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,52 @@ const express=require('express')
 const bodyParser = require('body-parser')
 const CryptoJS = require("crypto-js")
 const router=express.Router()
-const rsa=require('../rsafun');
+const rsa=require('../rsafun')
+var _ = require('lodash')
+
+var admin = require("firebase-admin");
+var serviceAccount = require("../cert/serviceAccountKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://bucketcrypt.firebaseio.com"
+});
+var db = admin.database()
+var ref = db.ref();
+var dataRef = ref.child("secretdata");
+
 
 router.post('/rsa/enc',function(req,res){
     if (!req.body) return res.sendStatus(400)
     var cipherText=rsa.encryptStringWithRsaPublicKey(req.body.text,'./cert/publicKey.PEM')
     console.log(cipherText.toString())
+    dataRef.push({
+        text: req.body.text,
+        cipher: cipherText.toString()
+      });
     res.send({
         Text: req.body.text,
         Cipher: cipherText.toString()
     });
 });
 
+router.get('/rsa/dec',function(req,res){
+    var result={};
+    var key="final";
+    result[key]=[];
+    dataRef.on("value", function(snapshot) {
+        var data = snapshot.val();
+        console.log(data);
+        for(el in data){
+            data[el].cipher=rsa.decryptStringWithRsaPrivateKey(data[el].cipher,'./cert/privateKey.PEM')
+           result[key].push(data[el]); 
+        }
+        console.log(JSON.stringify(result));
+        res.send(JSON.stringify(result));
+      });
+    // var plainText=rsa.decryptStringWithRsaPrivateKey(req.body.cipher,'./cert/privateKey.PEM')
+    // console.log(plainText)    
+    
+});
 
 router.post('/rsa/dec',function(req,res){
     if (!req.body) return res.sendStatus(400)
